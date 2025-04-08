a0de6ce4e7721d1dc9b3b4612017af6fba50baf5
Mario Cao <mariocao@gmail.com>
2025-04-08 10:57:14 +0000

    fix(commit): udpate commit functions

--- packages/contract/src/commit.test.ts
+++ packages/contract/src/commit.test.ts
@@ -1,31 +1,128 @@
 import { describe, expect, it } from "bun:test";
-import { createCommitmentHash, createCommitmentMessageSignatureHash } from "./commit";
+import { vrfProve } from "@sedaprotocol/overlay-ts-common/src/services/vrf";
+import { createCommitMessageHash, createCommitment, createRevealBodyHash, createRevealMessageHash } from "./commit";
+import type { RevealBody } from "./reveal";
+
+describe("createRevealBodyHash", () => {
+	it("should create the correct hash", () => {
+		const revealBody: RevealBody = {
+			dr_id: "3aa91e148d735de527a185f5ff36238dc4edae93605a1e0bb09962a2f64a818f",
+			dr_block_height: 1,
+			exit_code: 0,
+			gas_used: 1n,
+			reveal: Buffer.from("ccb1f717aa77602faf03a594761a36956b1c4cf44c6b336d1db57da799b331b8", "hex"),
+			proxy_public_keys: ["030123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"],
+		};
+		const hash = createRevealBodyHash(revealBody);
+
+		expect(hash.toString("hex")).toBe("6f1fcb82763ff3697546b945398c76e995acba40b9fa80c12b2ae27f990a4761");
+	});
+});
 
 describe("createCommitmentHash", () => {
 	it("should create the correct hash", () => {
-		const hash = createCommitmentHash({
+		const revealBody: RevealBody = {
+			dr_id: "3aa91e148d735de527a185f5ff36238dc4edae93605a1e0bb09962a2f64a818f",
+			dr_block_height: 1,
 			exit_code: 0,
-			gas_used: 0n,
-			id: "835793852eca9e6af588baca6611bb99fee61653e7306f5e9487b206e31639d2",
-			proxy_public_keys: [],
-			reveal: Buffer.from("1a192fabce13988b84994d4296e6cdc418d55e2f1d7f942188d4040b94fc57ac", "hex"),
-			salt: "9c0257114eb9399a2985f8e75dad7600c5d89fe3824ffa99ec1c3eb8bf3b0501",
-		});
+			gas_used: 1n,
+			reveal: Buffer.from("ccb1f717aa77602faf03a594761a36956b1c4cf44c6b336d1db57da799b331b8", "hex"),
+			proxy_public_keys: ["030123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"],
+		};
+		const revealBodyHash = createRevealBodyHash(revealBody);
+		const hash = createCommitment(
+			revealBodyHash,
+			"0300006a74b5d88b7abeae92d636168a20192d082c686cf9226667f6012e4e74e4",
+			"03aa10102500eab6fe3a1fabbecf5e62246ce5d328b666799804dfd78ea1396426a92e774a04e01fc72d453364b15300366d72354f7f3c450d6257e51e0779097a414b2ab1a7aba381d5947da193624a05",
+			[],
+			[],
+		);
 
-		expect(hash.toString("hex")).toBe("894dd8fa6be6b49dee0eab3df8408a4cb1fb5fcaa9bd1b851147efd7aa07ec1f");
+		expect(hash.toString("hex")).toBe("5cd42fbed0f93a8ad51098c3c3354203acbfd59ba67f0b1304a8db4938f4cba9");
 	});
 });
 
 describe("createCommitmentMessageSignatureHash", () => {
 	it("should create the correct hash", () => {
-		const messageHash = createCommitmentMessageSignatureHash(
-			"c1eada0eb326055dbda2331de47704279b7efdf1bdd73262ede12b844adc1977",
+		const messageHash = createCommitMessageHash(
+			"3aa91e148d735de527a185f5ff36238dc4edae93605a1e0bb09962a2f64a818f",
 			1n,
-			"30c57f91e77fd6a66617107af4830b757008e4a1b2d69a26c53f26656781d797",
+			"5cd42fbed0f93a8ad51098c3c3354203acbfd59ba67f0b1304a8db4938f4cba9",
 			"seda_test",
 			"seda1mzdhwvvh22wrt07w59wxyd58822qavwkx5lcej7aqfkpqqlhaqfsuj50sf",
 		);
 
-		expect(messageHash.toString("hex")).toBe("2de2397d143adffe2a37629bd34863beb616efbce18615bb9e42eca9a8f360c3");
+		expect(messageHash.toString("hex")).toBe("19d5058188ace3f0bff7511dba75f1ff17d2138c532bae63b711fc60a5756564");
+	});
+});
+
+describe("commit/reveal", () => {
+	it("should create the correct commit and reveal messages", () => {
+		const revealBody: RevealBody = {
+			dr_id: "3aa91e148d735de527a185f5ff36238dc4edae93605a1e0bb09962a2f64a818f",
+			dr_block_height: 1,
+			exit_code: 0,
+			gas_used: 1n,
+			reveal: Buffer.from("ccb1f717aa77602faf03a594761a36956b1c4cf44c6b336d1db57da799b331b8", "hex"),
+			proxy_public_keys: ["030123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"],
+		};
+
+		const signingKey = "2bd806c97f0e00af1a1fc3328fa763a9269723c8db8fac4f93af71db186d6e90";
+		const publicKey = "039997a497d964fc1a62885b05a51166a65a90df00492c8d7cf61d6accf54803be";
+		const chainId = "seda_test";
+		const contractAddr = "seda1mzdhwvvh22wrt07w59wxyd58822qavwkx5lcej7aqfkpqqlhaqfsuj50sf";
+		const stderr: string[] = [];
+		const stdout: string[] = [];
+
+		// Step 1: Create the reveal body hash
+		const revealBodyHash = createRevealBodyHash(revealBody);
+		expect(revealBodyHash.toString("hex")).toBe("6f1fcb82763ff3697546b945398c76e995acba40b9fa80c12b2ae27f990a4761");
+
+		// Step 2: Create the hash used as part of the reveal message
+		const revealMessageHash = createRevealMessageHash(revealBodyHash, chainId, contractAddr);
+		expect(revealMessageHash.toString("hex")).toBe("d964307485fdd72aa259e49d7465cc62bf3ac1bec77d796464c9b4c0ccde35f1");
+
+		// Step 3: Prove reveal message hash
+		const revealProof = vrfProve(Buffer.from(signingKey, "hex"), revealMessageHash);
+		expect(revealProof.toString("hex")).toBe(
+			"021b15623b1c54f9a3fc9c7de066732ddf90851c9a4a6997d1b68b387e95b6786843a05d256fb86f4bcb267f02fa168628ec89a24cccd2429b5a36760af575fb5ac6dca2e4beb4c53adae7953437b77f98",
+		);
+
+		// Step 4: Create Reveal Message
+		// const revealMessage = {
+		// 	revealBody: revealBody,
+		// 	publicKey: publicKey,
+		// 	proof: revealProof.toString("hex"),
+		// 	stderr: stderr,
+		// 	stdout: stdout,
+		// };
+
+		// Step 5: Commitment hash
+		const commitment = createCommitment(revealBodyHash, publicKey, revealProof.toString("hex"), stderr, stdout);
+		expect(commitment.toString("hex")).toBe("3c5f027221b68115e903218b21580e8eac4dc75400bcbb675a9b644353f2f8a8");
+
+		// Step 6: Create commit message hash
+		const commitMessageHash = createCommitMessageHash(
+			revealBody.dr_id,
+			BigInt(revealBody.dr_block_height),
+			commitment.toString("hex"),
+			chainId,
+			contractAddr,
+		);
+		expect(commitMessageHash.toString("hex")).toBe("8d339d64555776870046a41f1a5154c402672f5f5aa3c9c5a30dc298f20be8d8");
+
+		// Step 7: Prove commit message hash
+		const commitProof = vrfProve(Buffer.from(signingKey, "hex"), commitMessageHash);
+		expect(commitProof.toString("hex")).toBe(
+			"02b0a7baca1f08bb1de047dfda2c0c58818a565019a0aed9894430d072badc52ca07970536f97279ebd3567b3a7dcd147b212fe4c3b7de9cdee5105fdbc0807dbbeab8cacaedb2376949d6c1e4bb84a4fe",
+		);
+
+		// Step 8: Create Commit Message
+		// const commitMessage = {
+		// 	drId: revealBody.dr_id,
+		// 	commitment: commitment.toString("hex"),
+		// 	publicKey: publicKey,
+		// 	proof: commitProof.toString("hex"),
+		// };
 	});
 });
--- packages/contract/src/commit.ts
+++ packages/contract/src/commit.ts
@@ -1,11 +1,11 @@
 import { keccak256 } from "@sedaprotocol/overlay-ts-common";
 import type { RevealBody } from "./reveal";
 
-export function createCommitmentHash(revealBody: RevealBody): Buffer {
-	const revealHash = keccak256(revealBody.reveal);
-	const exitCodeBuffer = Buffer.from([revealBody.exit_code]);
+export function createRevealBodyHash(revealBody: RevealBody): Buffer {
+	const drBlockHeightBuffer = Buffer.alloc(8);
+	drBlockHeightBuffer.writeBigUInt64BE(BigInt(revealBody.dr_block_height));
 	const gasUsedBuffer = Buffer.alloc(8);
-	gasUsedBuffer.writeBigUInt64BE(revealBody.gas_used);
+	gasUsedBuffer.writeBigUInt64BE(BigInt(revealBody.gas_used));
 
 	const publicKeysHash = keccak256(
 		Buffer.concat(revealBody.proxy_public_keys.map((publicKey) => keccak256(Buffer.from(publicKey)))),
@@ -14,18 +14,52 @@ export function createCommitmentHash(revealBody: RevealBody): Buffer {
 	return Buffer.from(
 		keccak256(
 			Buffer.concat([
-				Buffer.from(revealBody.id, "hex"),
-				Buffer.from(revealBody.salt),
-				exitCodeBuffer,
+				Buffer.from(revealBody.dr_id, "hex"),
+				drBlockHeightBuffer,
+				Buffer.from([revealBody.exit_code]),
 				gasUsedBuffer,
-				revealHash,
+				keccak256(revealBody.reveal),
 				publicKeysHash,
 			]),
 		),
 	);
 }
 
-export function createCommitmentMessageSignatureHash(
+export function createRevealMessageHash(revealBodyHash: Buffer, chainId: string, contractAddr: string): Buffer {
+	return Buffer.from(
+		keccak256(
+			Buffer.concat([
+				Buffer.from("reveal_data_result"),
+				revealBodyHash,
+				Buffer.from(chainId),
+				Buffer.from(contractAddr),
+			]),
+		),
+	);
+}
+
+export function createCommitment(
+	revealBodyHash: Buffer,
+	publicKey: string,
+	proof: string,
+	stderr: string[],
+	stdout: string[],
+): Buffer {
+	return Buffer.from(
+		keccak256(
+			Buffer.concat([
+				Buffer.from("reveal_message"),
+				revealBodyHash,
+				Buffer.from(publicKey),
+				Buffer.from(proof),
+				Buffer.from(stderr.join("")),
+				Buffer.from(stdout.join("")),
+			]),
+		),
+	);
+}
+
+export function createCommitMessageHash(
 	dataRequestId: string,
 	dataRequestHeight: bigint,
 	commitment: string,
