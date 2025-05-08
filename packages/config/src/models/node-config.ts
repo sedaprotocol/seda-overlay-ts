aa4d18b8cb83a0f619d9d2aaf11c8c7321be362c
Franklin Waller <fr.l.waller@gmail.com>
2025-05-08 13:07:13 +0200

    feat(config): add support for configuring the amount of threads a node wants to use

--- packages/config/src/models/node-config.ts
+++ packages/config/src/models/node-config.ts
@@ -16,6 +16,7 @@ import {
 
 export const NodeConfigSchema = v.object({
 	forceSyncVm: v.optional(v.boolean(), DEFAULT_FORCE_SYNC_VM),
+	threadAmount: v.optional(v.number()),
 	terminateAfterCompletion: v.optional(v.boolean(), DEFAULT_TERMINATE_AFTER_COMPLETION),
 	maxConcurrentRequests: v.optional(v.number(), DEFAULT_MAX_CONCURRENT_REQUESTS),
 	maxGasLimit: v.optional(v.bigint(), DEFAULT_MAX_GAS_LIMIT),
