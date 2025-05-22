20cea884b8a670bedb917fe64151c0d8744b5bee
Franklin Waller <fr.l.waller@gmail.com>
2025-05-22 15:04:14 +0200

    feat: only log dr results in debug mode

--- packages/config/src/models/node-config.ts
+++ packages/config/src/models/node-config.ts
@@ -15,6 +15,7 @@ import {
 	DEFAULT_REQUEST_TIMEOUT,
 	DEFAULT_TERMINATE_AFTER_COMPLETION,
 	DEFAULT_TOTAL_HTTP_TIME_LIMIT,
+	DEFAULT_DEBUG,
 } from "../constants";
 
 export const NodeConfigSchema = v.object({
